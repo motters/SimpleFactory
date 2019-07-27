12a763c594eca2f8616087f7a2fcf5a61555ced6
motters <sammottleys@gmail.com>
2019-07-28 02:02:58 +0300

    improved example usage

--- main.cpp
+++ main.cpp
@@ -44,20 +44,32 @@ int main()
     // Create factory
     Platform::ManagerFactory<base> factory;
 
+
     // Create classes with auto naming
     factory.create<dog, true>();
     factory.create<cat, true>();
 
-    // Create another dog with a unique name of 2 and set the dog type within the class via the constructor
+    // Create another dog with a unique index of 2
+    // and set the dog type within the class via the constructor
     factory.create<dog>("2", "German Shepard");
 
+
+    // Validate the returned instance
+    auto catReturned = factory.get<cat>();
+    if(catReturned.status)
+    {
+        catReturned.instance->talk();
+    }
+
+
     // Speak
-    factory.get<cat>().instance->talk();
     factory.get<dog>().instance->talk();
     factory.get<dog>("2").instance->talk();
 
+
     // We dont have to ask for the instance, instead use ->
     factory.get<dog>()->talk();
+
 
     return 0;
 }
